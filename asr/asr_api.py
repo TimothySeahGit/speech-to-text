12a23169048eb891a9763866053baba6293c52d1
unknown <timothyseah.t.e@gmail.com>
2024-12-25 23:10:07 +0800

    get app working in docker

--- asr/asr_api.py
+++ asr/asr_api.py
@@ -77,4 +77,4 @@ async def create_upload_file(file: UploadFile):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", log_level="debug", port=PORT)  
+    uvicorn.run(app, host="0.0.0.0", log_level="debug", port=PORT)  
