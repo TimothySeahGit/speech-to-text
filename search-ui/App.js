3df9ce9f6d15c4fe01868378684666ed035fc4fa
unknown <timothyseah.t.e@gmail.com>
2024-12-27 12:07:33 +0800

    cleanup

--- search-ui/App.js
+++ search-ui/App.js
@@ -29,13 +29,6 @@ import "@elastic/react-search-ui-views/lib/styles/styles.css";
 //     getFacetFields
 // } from "./config/config-helper";
 
-// const { hostIdentifier, searchKey, endpointBase, engineName } = getConfig();
-// const connector = new AppSearchAPIConnector({
-//     searchKey,
-//     engineName,
-//     hostIdentifier,
-//     endpointBase
-// });
 const connector = new ElasticsearchAPIConnector({
     host: "http://localhost:9200", // The browser can only see containers from the outside!
     // host: "http://speech-to-text-elastic-backend-1:9200", # so, this is only for prod.
@@ -57,62 +50,20 @@ const config = {
             title: {
                 weight: 3
             },
-            // plot: {},
-            // genre: {},
-            // actors: {},
-            // directors: {}
             generated_text: {},
-            // duration: {},
             age: {},
             gender: {},
             accent: {}
         },
         result_fields: {
-            // title: {
-            //     snippet: {}
-            // },
-            // plot: {
-            //     snippet: {}
-            // }
             generated_text: {},
             duration: {},
             age: {},
             gender: {},
             accent: {}
         },
-        // disjunctiveFacets: ["genre.keyword", "actors.keyword", "directors.keyword"],
         disjunctiveFacets: ["age.keyword", "gender.keyword", "accent.keyword"],
         facets: {
-            // "genre.keyword": { type: "value" },
-            // "actors.keyword": { type: "value" },
-            // "directors.keyword": { type: "value" },
-            // released: {
-            //     type: "range",
-            //     ranges: [
-            //         {
-            //             from: "2012-04-07T14:40:04.821Z",
-            //             name: "Within the last 10 years"
-            //         },
-            //         {
-            //             from: "1962-04-07T14:40:04.821Z",
-            //             to: "2012-04-07T14:40:04.821Z",
-            //             name: "10 - 50 years ago"
-            //         },
-            //         {
-            //             to: "1962-04-07T14:40:04.821Z",
-            //             name: "More than 50 years ago"
-            //         }
-            //     ]
-            // },
-            // imdbRating: {
-            //     type: "range",
-            //     ranges: [
-            //         { from: 1, to: 3, name: "Pants" },
-            //         { from: 3, to: 6, name: "Mediocre" },
-            //         { from: 6, to: 8, name: "Pretty Good" },
-            //         { from: 8, to: 10, name: "Excellent" }
-            //     ]
-            // }
             "duration": {
                 type: "range",
                 ranges: [
@@ -155,7 +106,7 @@ const config = {
         }
     },
     apiConnector: connector,
-    alwaysSearchOnInitialLoad: false
+    alwaysSearchOnInitialLoad: true
 };
 
 export default function App() {
@@ -164,41 +115,6 @@ export default function App() {
             <WithSearch mapContextToProps={({ wasSearched }) => ({ wasSearched })}>
                 {({ wasSearched }) => {
                     return (
-                        // <div className="App">
-                        //     <ErrorBoundary>
-                        //         <Layout
-                        //             header={<SearchBox autocompleteSuggestions={true} />}
-                        //             sideContent={
-                        //                 <div>
-                        //                     {wasSearched && (
-                        //                         <Sorting
-                        //                             label={"Sort by"}
-                        //                             sortOptions={buildSortOptionsFromConfig()}
-                        //                         />
-                        //                     )}
-                        //                     {getFacetFields().map(field => (
-                        //                         <Facet key={field} field={field} label={field} />
-                        //                     ))}
-                        //                 </div>
-                        //             }
-                        //             bodyContent={
-                        //                 <Results
-                        //                     titleField={getConfig().titleField}
-                        //                     urlField={getConfig().urlField}
-                        //                     thumbnailField={getConfig().thumbnailField}
-                        //                     shouldTrackClickThrough={true}
-                        //                 />
-                        //             }
-                        //             bodyHeader={
-                        //                 <React.Fragment>
-                        //                     {wasSearched && <PagingInfo />}
-                        //                     {wasSearched && <ResultsPerPage />}
-                        //                 </React.Fragment>
-                        //             }
-                        //             bodyFooter={<Paging />}
-                        //         />
-                        //     </ErrorBoundary>
-                        // </div>
                         <div className="App">
                             <ErrorBoundary>
                                 <Layout
